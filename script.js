baa86641b12ecd33053f0a965353f858cce1938e
SrNester <134897941+SrNester@users.noreply.github.com>
2025-07-20 20:42:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const bgAnimation = document.getElementById('bgAnimation');
 
-const numberOfColorBoxes = 1200;
+const numberOfColorBoxes = 12000;
 
 for (let i = 0; i < numberOfColorBoxes; i++) {
     const colorBox = document.createElement('div');
